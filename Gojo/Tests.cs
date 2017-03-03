239a14da4d9fb8bf3bbd3932bd4905097a7dbbcf
JuneCho23 <junecho.ucb@gmail.com>
2017-03-03 10:13:29 -0800

    Updated Test Scripts

--- Gojo/Tests.cs
+++ Gojo/Tests.cs
@@ -33,16 +33,19 @@ namespace Gojo
 		}
 
 		[Test]
-		public void FirstTest()
+		public void DemoHospitalFiveMoments()
 		{
 			app.Tap("login_username");
 			app.EnterText("demo@gojo.com");
-
 			app.Tap("login_password");
 			app.EnterText("smartlink");
+			app.Tap("login_button");
 
+			app.Tap("home_start_session_button");
+			app.Tap("Demo Hospital - Five Moments");
 
-
+			app.Tap("activity_observation_type_select_type");
+			app.Tap("dialog_double_button_ok_button")
 
 		}
 
